1ee956a41ae0a0d45d3cf022d5511d4df3000bda
Nur Seda Aggunlu <nursedaaggunlu@gmail.com>
2025-10-01 16:53:19 +0300

    yeniden yenilendi

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,5 @@
 function formatMessage(message, maxLength) {
-  if (message.maxLength <= maxLength.maxLength) {
+  if (message.maxLength <= maxLength) {
     return message;
   } else {
     return message.slice(0, maxLength) + "...";
